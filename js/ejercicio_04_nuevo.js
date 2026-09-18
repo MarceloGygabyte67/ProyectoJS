8350adc47e5f2099f2e3659efd6fb8c7283300ef
Marcelo Ramirez <ramirezolamarcelo546@gmail.com>
2026-09-18 10:56:45 -0500

    He creado un nuevo archivo JS

--- js/ejercicio_04_nuevo.js
+++ js/ejercicio_04_nuevo.js
@@ -1,10 +1,26 @@
-let NumerosEnterosPositivos = "1,5,10"
-let Suma = 6
-let Resta = 4
-let Multiplicacion = 2
+//NUMEROS ENTEROS//
+let PrimerEntero = 1;   
+let SegundoEntero = 5;
+let TercerEntero = 10;
 
-let Mostracion = 1*5*10 + 6 
-const = 
+let suma = PrimerEntero + SegundoEntero + TercerEntero;
+let resta = PrimerEntero - SegundoEntero - TercerEntero;
+let multiplicacion = PrimerEntero * SegundoEntero * TercerEntero;
+
+console.log(":::::::::PROGRAMA:::::::::");
+console.log("Primer Entero: ", PrimerEntero );
+console.log("Segundo Entero: ", SegundoEntero);
+console.log("Tercer Entero: ", TercerEntero);
+console.log("Suma de los Enteros: ", suma);
+console.log("Resta de los Enteros: ", resta);
+console.log("Multiplicacion de los Enteros: ", multiplicacion);
+
+
+
+
+
+
+
 
 
 
