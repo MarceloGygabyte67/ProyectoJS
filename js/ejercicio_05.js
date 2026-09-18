a3df305de6d050b8f425b2f3d323c75cc8fc6d65
Marcelo Ramirez <ramirezolamarcelo546@gmail.com>
2026-09-18 09:03:32 -0500

    He desarrollado un nuevo archivo JS

---
+++ js/ejercicio_05.js
@@ -0,0 +1,20 @@
+let Usuario = "Wasaberto";
+let KilometrosRecorridos = 60;
+let CombustibleConsumido = 6;
+
+let RecorridoMotocicleta = KilometrosRecorridos * CombustibleConsumido; 360
+const CantidadLitros = 0.10 * RecorridoMotocicleta; 36
+let CombustibleKilometro = RecorridoMotocicleta - CantidadLitros; 324
+
+console.log(":::::::::KILOMETROS RECORRIDOS:::::::::")
+console.log("Nombre de Usuario: ", Usuario);
+console.log("Kilometros Recorridos: ", KilometrosRecorridos);
+console.log("Combustible Consumido Durante Recorrido: ", CombustibleConsumido);
+console.log("Recorrido por Motocicleta: ", RecorridoMotocicleta),
+console.log("Cantidad de Litros: ", CantidadLitros),
+console.log("Combustible por Kilometro: ", CombustibleKilometro);
+    
+
+
+
+
